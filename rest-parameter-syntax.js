060bf945b19ce7246000869048d92e0b5aed8f8d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-10-07 12:35:54 +0000

    use template to learn about rest parameter syntax

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -5,6 +5,31 @@
 
 // Regular function call 
 
+const sumAll = (x, y, z) => (x + y + z);
+
+let total = sumAll(5, 5, 5);
+
+console.log('Total:', total);
+
 // Extra arguments are ignored
 
+let total2 = sumAll(5, 5, 5, 5, 5, 5);
+
+console.log('Total:', total2);
+
+
 // Function using ...rest
+
+let sumAlll = (x, y, z, ...rest) => {
+
+    let sum = x + y + z;
+    for (let i of rest) {
+      sum += i
+    } 
+
+    return sum
+};
+
+let sum3 = sumAlll(5, 5, 5, 5, 5);
+
+console.log('Sum3:', sum3);
